0dd2d4a76e0e5ef55e255bb58efad384a9fbdad5
pandascrushiva <velayuthamsiva55@gmail.com>
2024-04-12 20:27:01 +0530

    feat : Authentication Project

--- routes/userroute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { userRegister, loginUser } = require('../controller/usercontroller')
-const userRouter = express.Router()
-
-userRouter.route('/reg').post(userRegister)
-userRouter.route('/login').post(loginUser)
-
-module.exports = {userRouter}
